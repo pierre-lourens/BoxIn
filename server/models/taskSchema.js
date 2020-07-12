d03638d3a1f725733848dd1e35191d309ff9491c
Pierre Lourens <plourens3@gmail.com>
2020-07-12 17:04:32 -0400

    Stashing mongo work; switching to SQL

--- server/models/taskSchema.js
+++ server/models/taskSchema.js
@@ -6,10 +6,12 @@ const taskSchema = new Schema(
     text: String,
     scheduledDate: String,
     status: String,
-    tags: Array,
     timeEstimate: String,
     actualTime: String,
     weight: String,
+    tags: [tagSchema],
+    projects: [{ type: Schema.Types.ObjectId, ref: "project" }],
+    userID: { type: Schema.Types.ObjectId, ref: "user" },
   },
   { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
 );
