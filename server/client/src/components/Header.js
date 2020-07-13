844b08ba48d596acf3ecb5d3668f515d565bc23c
Pierre Lourens <plourens3@gmail.com>
2020-07-13 11:54:41 -0400

    Fixes responsiveness for logo & input bar

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import MainTaskAdder from "./MainTaskAdder";
-import Navigation from "./Navigation";
 
 const Wrapper = styled.div`
   display: grid;
   grid-gap: 1rem;
-  grid-template-columns: repeat(12, 1fr);
+  grid-template-columns: repeat(8, 1fr);
   grid-template-rows: 100px;
   background-color: ${(props) => props.theme.colors.darkBlue};
   box-shadow: 0 4px 6px 0 rgba(100, 100, 100, 0.2);
@@ -15,15 +14,16 @@ const Wrapper = styled.div`
 `;
 
 const StyledHeader = styled.header`
-  grid-column: span 3;
+  grid-column: span 2;
   grid-row: 1;
+  justify-self: end;
 `;
 
 const Logo = styled.h1`
   font-family: ${(props) => props.theme.headingFonts.toString()};
   padding: 0;
-  margin: 0;
-  color: white};
+  margin: 0px;
+  color: white;
   span {
     border: 2px solid white;
     padding: 3px 6px;
@@ -43,8 +43,8 @@ const Header = () => {
             <span>Box</span>Out
           </Logo>
         </StyledHeader>
+
         <MainTaskAdder />
-        <Navigation />
       </Wrapper>
     </React.Fragment>
   );
--- server/client/src/components/MainTaskAdder.js
+++ server/client/src/components/MainTaskAdder.js
@@ -3,14 +3,14 @@ import styled from "styled-components";
 
 const StyledInputContainer = styled.div`
   grid-row: 1;
-  grid-column: span 6;
+  grid-column: span 4;
   background-color: ${(props) => props.theme.colors.darkBlue};
   width: 100%;
 
   input {
     height: 30px;
     padding: 5px 10px;
-    width: 90%;
+    width: 100%;
   }
 `;
 
