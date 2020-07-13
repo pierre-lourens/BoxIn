4c1585795d9424f48226d8a4d15ce707fa194944
Pierre Lourens <plourens3@gmail.com>
2020-07-13 08:46:08 -0400

    Reduces react clutter; adds styled components

---
+++ server/client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.header``;
+
+const Header = () => {
+  return <StyledHeader>Hi</StyledHeader>;
+};
+
+export default Header;
